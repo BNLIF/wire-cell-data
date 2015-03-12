0dfa0b7afaab570515bc3bb9cfe71015cb53e904
Brett Viren <brett.viren@gmail.com>
2015-03-11 21:35:07 -0400

    Test various parts of the API.

--- test/test_api.py
+++ test/test_api.py
@@ -13,6 +13,44 @@ def test_make_wire():
     assert val
     print val
 def test_make_cell():
-    val = WireCell().Id()
+    val = WireCell().Cell()
     assert val
     print val
+
+def test_make_point():
+    Point = WireCell().Point
+    PointVector = WireCell().PointVector
+    assert Point()
+    p = Point(1.0,2.0)
+    assert p
+    assert p.first == 1.0
+    assert p.second == 2.0
+    p2 = Point(2,4)
+    assert p2.first == 2.0
+    assert p2.second == 4.0
+    pv = PointVector()
+    assert pv
+    pv.push_back(p)
+    pv.push_back(p2)
+    assert pv.size() == 2
+    pp = pv[0]
+    assert pp
+    assert pp.first == 1.0
+    assert pp.second == 2.0
+    pp2 = pv[1]
+    assert pp2
+    assert pp2.first == 2
+    assert pp2.second == 4
+    
+    pt = tuple([(x.first,x.second) for x in pv])
+    assert pt == ((1.0,2.0),(2.0,4.0))
+
+
+def test_units():
+    import ROOT
+    assert ROOT.units.mm == 1.0
+    assert ROOT.units.radian == 1.0
+    assert ROOT.units.ns == 1.0
+    assert ROOT.units.kelvin == 1.0
+    assert ROOT.units.mole == 1.0
+    assert ROOT.units.MeV == 1.0
