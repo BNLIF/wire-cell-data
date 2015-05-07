11f9947d65dd4a3189b0c6b73602ee4196ece3a0
Brett Viren <bv@bnl.gov>
2015-05-07 15:35:36 -0400

    Add SimTruth data class.

---
+++ src/SimTruth.cxx
@@ -0,0 +1,13 @@
+#include "WireCellData/SimTruth.h"
+
+using namespace WireCell;
+
+SimTruth::SimTruth(float x, float y, float z, float q, int tdc, int trackid)
+    : _x(x), _y(y), _z(z), _q(q), _tdc(tdc), _trackid(trackid)
+{
+}
+SimTruth::~SimTruth()
+{
+}
+
+
