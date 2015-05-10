1fba32c11c5b9805f3a7346f243f5972eac3b0de
Xin Qian <last.qx@gmail.com>
2015-05-10 18:07:54 -0400

    add a Merged Wire class

---
+++ inc/WireCellData/MergeGeomWire.h
@@ -0,0 +1,21 @@
+#ifndef GeomWireCellData_MergeWire_h
+#define GeomWireCellData_MergeWire_h
+
+#include "WireCellData/GeomWire.h"
+namespace WireCell{
+  class MergeGeomWire : public WireCell::GeomWire{
+  public:
+    
+    MergeGeomWire(int ident, const WireCell::GeomWire& wire);
+    MergeGeomWire(int ident, const WireCell::MergeGeomWire& wire);
+
+    int AddWire(const WireCell::GeomWire& wire);
+    int AddWire(WireCell::MergeGeomWire& wire);
+    
+    WireCell::GeomWireSelection get_allwire() const{ return wire_all;}
+  protected:
+    WireCell::GeomWireSelection wire_all;
+  };
+}
+
+#endif
---
+++ src/MergeGeomWire.cxx
@@ -0,0 +1,64 @@
+#include "WireCellData/MergeGeomWire.h"
+
+#include <vector>
+#include <cmath>
+using namespace std;
+using namespace WireCell;
+
+MergeGeomWire::MergeGeomWire(int ident, const WireCell::GeomWire& wire)
+{
+  _ident = ident;
+  _plane = wire.plane();
+  wire_all.push_back(&wire);
+}
+
+MergeGeomWire::MergeGeomWire(int ident, const WireCell::MergeGeomWire& wire)
+{
+  _ident = ident;
+  _plane = wire.plane();
+  wire_all = wire.get_allwire();
+}
+
+int MergeGeomWire::AddWire(const WireCell::GeomWire& wire){
+  wire_all.push_back(&wire);
+  return 0;
+}
+
+int MergeGeomWire::AddWire(WireCell::MergeGeomWire& wire){
+  GeomWireSelection wires2 = wire.get_allwire();
+  int flag = 0;
+  const GeomWire *swire1, *swire2;
+
+  for (int i=0;i!=wire_all.size();i++){
+    swire1 = wire_all[i];
+    for (int j=0;j!=wires2.size();j++){
+      swire2 = wires2[j];
+
+      if(swire1 == swire2){
+	flag = 1;
+	break;
+      }
+    }
+    if (flag==1) break;
+  }
+
+  if (flag==1){
+    
+    for (int j=0;j!=wires2.size();j++){
+      int flag1 = 0;
+      swire2 = wires2[j];
+      for (int i=0;i!=wire_all.size();i++){
+	swire1 = wire_all[i];
+	if (swire1 == swire2){
+	  flag1 = 1;
+	  break;
+	}
+      }
+      if (flag1==0) wire_all.push_back(swire2);
+    }
+
+    return 1;
+  }else{
+    return 0;
+  }
+}
