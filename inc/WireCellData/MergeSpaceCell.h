5f9e67f82fd449c6019c94ae350d539f6aded979
Xin Qian <last.qx@gmail.com>
2015-07-26 23:22:59 -0400

    add calculation center method for MergeSpacePoint

--- inc/WireCellData/MergeSpaceCell.h
+++ inc/WireCellData/MergeSpaceCell.h
@@ -1,6 +1,6 @@
 #ifndef MergeSpaceWireCellData_Cell_h
 #define MergeSpaceWireCellData_Cell_h
-
+#include "WireCellData/Point.h"
 #include "WireCellData/SpaceCell.h"
 
 #include <set>
@@ -14,14 +14,17 @@ namespace WireCell {
      */
     class MergeSpaceCell {
     public:
-      MergeSpaceCell(){};
+      MergeSpaceCell(){center_flag = 0;};
       ~MergeSpaceCell(){};
 	
       void AddSpaceCell(SpaceCell* cell){all_spacecell.push_back(cell);};
       SpaceCellSelection& Get_all_spacecell(){return all_spacecell;};
-	
-    protected:
       
+      Point& Get_Center();
+
+    protected:
+      int center_flag;
+      Point center;
       SpaceCellSelection all_spacecell;
       
 };
