63ae610304b857187f856984050127dedc66e1e5
Xin Qian <last.qx@gmail.com>
2015-08-12 10:27:49 -0400

    prepare for tracking

--- inc/WireCellData/WCTrack.h
+++ inc/WireCellData/WCTrack.h
@@ -1,12 +1,15 @@
 #ifndef WCTrack_h
 #define WCTrack_h
 
+#include "WireCellData/MergeClusterTrack.h"
+
 namespace WireCell {
   class WCTrack{
   public:
-    
+    WCTrack(MergeClusterTrack& mct);
   protected:
-    
+    int track_type;
+    MergeClusterTrack& mct;
   };
 }
 
---
+++ src/WCTrack.cxx
@@ -0,0 +1,8 @@
+#include "WireCellData/WCTrack.h"
+
+using namespace WireCell;
+
+WCTrack::WCTrack(MergeClusterTrack& mct)
+  : mct(mct)
+{
+}
