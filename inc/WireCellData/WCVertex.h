4fdfeeb98461b3fd386bce34c2410a49c76b33fb
Xin Qian <last.qx@gmail.com>
2015-08-15 16:28:55 -0400

    add in a chi2 function for vertex fitting

--- inc/WireCellData/WCVertex.h
+++ inc/WireCellData/WCVertex.h
@@ -5,6 +5,12 @@
 #include "WireCellData/MergeSpaceCell.h"
 #include "WireCellData/WCTrack.h"
 
+//#include "Minuit2/Minuit2Minimizer.h"
+//#include "Math/Functor.h"
+#include "Minuit2/FCNBase.h"
+
+
+
 #include <vector>
 #include <map>
 
@@ -27,17 +33,44 @@ namespace WireCell {
     WCTrackSelection BreakTracks();
     void ProcessTracks(WCTrackSelection& break_tracks);
     void OrganizeEnds(MergeSpaceCellSelection& cells, int flag = 1);
-
+    // static double dis2(const double *xx);
+    int FindVertex();
+    
+    double get_ky(int i){return tracks_ky.at(i);}
+    double get_kz(int i){return tracks_kz.at(i);}
+    
 
   protected:
     Point center;
     MergeSpaceCell *msc;
     WCTrackSelection tracks;
+    std::vector<double> tracks_ky;
+    std::vector<double> tracks_kz;
   };
   
   typedef std::vector<WCVertex*> WCVertexSelection;
   typedef std::map<MergeSpaceCell*, WCVertex*> MSC_WCV_Map;
   //typedef std::map<WCTrack*, WCVertexSelection> WCT_WCVs_Map;
+
+
+  class MyFCN : public ROOT::Minuit2::FCNBase { 
+    
+  public: 
+    double Up() const { return 1.; }
+    
+    MyFCN(WCVertex* vertex) 
+      : vertex(vertex)
+      {}
+    
+    ~MyFCN(){}
+        
+    double operator() (const std::vector<double> & xx) const;
+    double get_chi2(const std::vector<double> & xx) const;
+
+  private:
+    WCVertex *vertex;
+  };
+
 }
 
 #endif
--- src/WCVertex.cxx
+++ src/WCVertex.cxx
@@ -1,5 +1,14 @@
 #include "WireCellData/WCVertex.h"
 
+
+#include "Minuit2/FunctionMinimum.h"
+#include "Minuit2/MnUserParameterState.h"
+#include "Minuit2/MnMigrad.h"
+//#include "Minuit2/MnMinos.h"
+//#include "Minuit2/MnContours.h"
+//#include "Minuit2/MnPlot.h"
+#include "TVector3.h"
+
 using namespace WireCell;
 
 WCVertex::WCVertex(MergeSpaceCell& msc)
@@ -10,6 +19,222 @@ WCVertex::WCVertex(MergeSpaceCell& msc)
 
 WCVertex::~WCVertex(){
 }
+
+
+
+//   return 0;
+// }
+
+double MyFCN::get_chi2(const std::vector<double> & xx) const{
+  double chi2 = 0;
+  double charge = 0;
+
+  int ntracks = vertex->get_tracks().size();
+  double x,y,z;
+  double ky[100],kz[100];
+  
+  if (ntracks==1){
+    x = vertex->get_msc()->Get_Center().x;
+    y = xx[0];
+    z = xx[1];
+    ky[0] = xx[2];
+    kz[0] = xx[3];
+  }else{
+    x = xx[0];
+    y = xx[1];
+    z = xx[2];
+    for (int i=0;i!=ntracks;i++){
+      ky[i] = xx[3+2*i];
+      kz[i] = xx[3+2*i+1];
+    }
+  }
+
+  double x0 = vertex->get_msc()->Get_Center().x;
+
+  for (int i=0;i!=ntracks;i++){
+    WCTrack *track = vertex->get_tracks().at(i);
+    MergeSpaceCellSelection cells = track->get_all_cells();
+    for (int j=0;j!=cells.size();j++){
+      MergeSpaceCell *mscell = cells.at(j);
+      double xc = mscell->Get_Center().x;
+      
+      int flag = 0;
+      if (ntracks==1){
+	if (fabs(xc-x0)/units::cm > 0.1 && fabs(xc-x0)/units::cm < 5)
+	  flag = 1;
+      }else if (ntracks>1){
+	if (fabs(xc-x0)/units::cm > 0.7 && fabs(xc-x0)/units::cm < 5)
+	  flag = 1;
+	
+      }
+      
+      if (flag == 1 ){
+	// 1.0-5 cm
+	
+	//std::cout << xc/units::cm << " " << x0/units::cm << std::endl;
+
+	SpaceCellSelection scells = mscell->Get_all_spacecell();
+	for (int k=0;k!=scells.size();k++){
+	  double x1 = scells.at(k)->x();
+	  double y1 = scells.at(k)->y();
+	  double z1 = scells.at(k)->z();
+	  double q = scells.at(k)->q();
+
+	  double a,b,c,d;
+	  
+	  a = y - ky[i]*x;
+	  b = ky[i];
+	  c = z - kz[i]*x;
+	  d = kz[i];
+
+	  double x2 = (-a*b-c*d+b*y1+d*z1+x1)/(1+b*b+d*d);
+	  double y2 = a + b*x2;
+	  double z2 = c + d*x2;
+
+	  // std::cout << x1/units::cm << " " << x2/units::cm << " " << 
+	  //   y1/units::cm << " " << y2/units::cm << " " << 
+	  //   z1/units::cm << " " << z2/units::cm << " " <<  q << 
+	  //   std::endl;
+
+	  
+
+	  // TVector3 n(1,ky[i],kz[i]);
+	  // TVector3 p(x-x1,y-x1,z-x1);
+	  // TVector3 v = p.Cross(n);
+	  // double dis = v.Mag()/n.Mag();
+	  
+	  chi2 += (pow(x2-x1,2)+pow(y2-y1,2)+pow(z2-z1,2))*q/0.15/0.15*3./units::cm/units::cm;
+	  charge += q;
+	}
+      }
+    }
+  }
+
+  //std::cout << chi2 << " " << charge << std::endl;
+  
+  return chi2/charge;
+}
+
+double MyFCN::operator() (const std::vector<double> & xx) const{
+  return get_chi2(xx);
+  
+} 
+
+
+int WCVertex::FindVertex(){
+
+
+
+  MyFCN fcn(this);
+  int ntracks = tracks.size();
+  
+  std::vector<double> variable;
+  int npar;
+  
+  if (ntracks == 1){
+    // 4 parameters, (2 for vertex y,z) and two directions
+    npar = 4;
+    variable.push_back(center.y);
+    variable.push_back(center.z);
+    
+    WCTrack* track = tracks.at(0);
+    MergeSpaceCell *a1 = track->get_end_scells().at(0);
+    MergeSpaceCell *a2 = track->get_end_scells().at(1);
+
+    // float dis1 = fabs(a1->Get_Center().x-center.x);
+    // float dis2 = fabs(a2->Get_Center().x-center.x);
+    float ky,kz;
+    
+    ky = (a2->Get_Center().y-a1->Get_Center().y)/(a2->Get_Center().x-a1->Get_Center().x);
+    kz = (a2->Get_Center().z-a1->Get_Center().z)/(a2->Get_Center().x-a1->Get_Center().x);
+    
+    //std::cout << "abc: " << center.y/units::cm << " " << center.z/units::cm << " " << ky << " " << kz << std::endl; 
+    
+    variable.push_back(ky);
+    variable.push_back(kz);
+  }else{
+    // 3 parameters for vertex and 2*n for track direction, each for a track
+    npar = 3 + 2*ntracks;
+
+    variable.push_back(center.x);
+    variable.push_back(center.y);
+    variable.push_back(center.z);
+    
+    for (int i=0;i!=ntracks;i++){
+      WCTrack* track = tracks.at(i);
+      MergeSpaceCell *a1 = track->get_end_scells().at(0);
+      MergeSpaceCell *a2 = track->get_end_scells().at(1);
+      
+      
+      float ky,kz;
+      
+      ky = (a2->Get_Center().y-a1->Get_Center().y)/(a2->Get_Center().x-a1->Get_Center().x);
+      kz = (a2->Get_Center().z-a1->Get_Center().z)/(a2->Get_Center().x-a1->Get_Center().x);
+     
+      //std::cout << "abc: " << ky << " " << kz << std::endl; 
+      
+      variable.push_back(ky);
+      variable.push_back(kz);
+    }
+  }
+  
+  ROOT::Minuit2::MnUserParameters upar;
+  for (int i=0;i!=npar;i++){
+    upar.Add(Form("x_%d",i),variable.at(i),0.001);
+  }
+  // create MIGRAD minimizer
+  ROOT::Minuit2::MnMigrad migrad(fcn, upar);
+
+  // minimize
+  ROOT::Minuit2::FunctionMinimum min = migrad();
+  const ROOT::Minuit2::MnUserParameterState& state = min.UserState();
+  
+  tracks_ky.clear();
+  tracks_kz.clear();
+  if (ntracks == 1){
+    center.y = state.Value(0);
+    center.z = state.Value(1);
+    tracks_ky.push_back(state.Value(2));
+    tracks_kz.push_back(state.Value(3));
+  }else{
+    center.x = state.Value(0);
+    center.y = state.Value(1);
+    center.z = state.Value(2);
+
+    
+
+    for (int i=0;i!=ntracks;i++){
+      tracks_ky.push_back(state.Value(3+2*i));
+      tracks_kz.push_back(state.Value(3+2*i+1));
+
+      //std::cout << state.Value(3+2*i) << " " << state.Value(3+2*i+1) << std::endl;
+    }
+  }
+  
+  // // variable[0] = 62.0792 * units::cm;
+  // // variable[1] = 35 * units::cm;
+  // // variable[2] = 636*units::cm;
+
+  // std::vector<double> variable1;
+  // variable1.push_back(state.Value(0));
+  // variable1.push_back(state.Value(1));
+  // variable1.push_back(state.Value(2));
+  // variable1.push_back(state.Value(3));
+
+  // output
+  //  std::cout << fcn.get_chi2(variable) << " Minimum: " << min.Fval() << " " << state.Value(0)/units::cm << " " <<state.Value(1)/units::cm << " " << state.Value(2)/units::cm << std::endl;
+  // std::cout << "abc1: " << min.Fval() << " " << fcn.get_chi2(variable) << " " << fcn.get_chi2(variable1) << " " << state.Value(0)/units::cm << " " <<state.Value(1)/units::cm  << " " << state.Value(2) << " " << state.Value(3) << std::endl;
+
+  //std::cout << fcn.get_chi2(variable) << std::endl;
+
+
+  
+
+ 
+   return 0;
+}
+
+
 
 
 Point WCVertex::Center(){
