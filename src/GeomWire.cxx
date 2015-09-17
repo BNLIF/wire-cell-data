959655af69d1e2f89dee4df4d06be6e440f97a30
Xiaoyue Li <xiaoyue.li@stonybrook.edu>
2015-09-17 15:25:58 -0400

    GeomWire now has a cryostat index

--- src/GeomWire.cxx
+++ src/GeomWire.cxx
@@ -5,6 +5,7 @@
 
 using namespace WireCell;
 
+/*
 GeomWire::GeomWire(int ident,
 		   WirePlaneType_t plane,
 		   int index,
@@ -25,6 +26,31 @@ GeomWire::GeomWire(int ident,
     , _apa(apa)
 {
 }
+*/
+
+GeomWire::GeomWire(unsigned int ident,
+		   WirePlaneType_t plane,
+		   int index,
+		   int channel,
+		   const Point& point1,
+		   const Point& point2,
+		   char segment,
+		   char face,
+		   short apa,
+		   short cryo)
+    : _ident(ident)
+    , _plane(plane)
+    , _index(index)
+    , _channel(channel)
+    , _point1(point1)
+    , _point2(point2)
+    , _segment(segment)
+    , _face(face)
+    , _cryo(cryo)
+    , _apa(apa)
+{
+}
+
 
 GeomWire::~GeomWire()
 {
@@ -40,7 +66,8 @@ std::ostream & WireCell::operator<<(std::ostream &os, const GeomWire& gw)
 	      << "a:" << gw.apa() << " "
 	      << "f:" << gw.face() << " "
 	      << "i:" << gw.index() << " "
-	      << "s:" << gw.segment() << ">";
+	      << "s:" << gw.segment() << ""
+	      << "c: "<< gw.cryo() << ">";
 }
 
 
