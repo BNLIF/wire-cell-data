4db92e9ad235059586a3e17259acd1a5e9ae10ea
Xin Qian <last.qx@gmail.com>
2015-12-20 12:42:04 -0500

    improve the precision to calculate the center coordinate for merged space cell

--- src/MergeSpaceCell.cxx
+++ src/MergeSpaceCell.cxx
@@ -250,20 +250,38 @@ Point& MergeSpaceCell::Get_Center(){
   if (center_flag==1){
     return center;
   }else{
-    center.x = 0;
-    center.y = 0;
-    center.z = 0;
+    // center.x = 0;
+    // center.y = 0;
+    // center.z = 0;
     
+    // for (int i=0;i!=all_spacecell.size();i++){
+    //   SpaceCell* cell = all_spacecell.at(i);
+    //   center.x += cell->x();
+    //   center.y += cell->y();
+    //   center.z += cell->z();
+    // }
+    
+    // center.x /= all_spacecell.size();
+    // center.y /= all_spacecell.size();
+    // center.z /= all_spacecell.size();
+
+    double x = 0;
+    double y = 0;
+    double z = 0;
     for (int i=0;i!=all_spacecell.size();i++){
       SpaceCell* cell = all_spacecell.at(i);
-      center.x += cell->x();
-      center.y += cell->y();
-      center.z += cell->z();
+      x += cell->x();
+      y += cell->y();
+      z += cell->z();
     }
-    
-    center.x /= all_spacecell.size();
-    center.y /= all_spacecell.size();
-    center.z /= all_spacecell.size();
+    x /= all_spacecell.size();
+    y /= all_spacecell.size();
+    z /= all_spacecell.size();
+
+    center.x = x;
+    center.y = y;
+    center.z = z;
+
     
     return center;
   }
