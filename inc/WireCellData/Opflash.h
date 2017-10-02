1295ba49037d1110c1810cd215db643302ea895d
Xin Qian <last.qx@gmail.com>
2017-10-01 20:38:55 -0400

    work on flashes

--- inc/WireCellData/Opflash.h
+++ inc/WireCellData/Opflash.h
@@ -2,11 +2,14 @@
 #define Opflash_h
 
 #include "WireCellData/COphit.h"
+#include "TH1F.h"
 
 namespace WireCell{
   class Opflash{
   public:
     Opflash(COphitSelection &ophits);
+    Opflash(TH1F **hist, double start_time, int start_bin, int end_bin, float bin_width=6*15.625/1000.);
+	    
     ~Opflash();
 
     double get_time(){return time;};
@@ -15,8 +18,17 @@ namespace WireCell{
     double get_PE_err(int ch){return PE_err[ch];};
     bool get_fired(int ch);
     int get_num_fired(){return fired_channels.size();};
+
+    int get_type(){return type;}
+    double get_low_time(){return low_time;};
+    double get_high_time(){return high_time;};
     
   protected:
+    
+    int type;
+    double low_time;
+    double high_time;
+    
     double time;
     double total_PE;
     std::vector<int> fired_channels;
--- src/Opflash.cxx
+++ src/Opflash.cxx
@@ -2,7 +2,9 @@
 
 using namespace WireCell;
 
-WireCell::Opflash::Opflash(COphitSelection &ophits){
+WireCell::Opflash::Opflash(COphitSelection &ophits)
+  : type(1)
+{
   for (int i=0;i!=32;i++){
     PE[i] = 0;
     PE_err[i] = 4.6;
@@ -17,6 +19,64 @@ WireCell::Opflash::Opflash(COphitSelection &ophits){
     total_PE += ophits.at(i)->get_PE() ;
   }
   time /= total_PE;
+
+  low_time = time - 3 * 15.625/1000.;
+  high_time = time + 37 * 15.625/1000.;
+  
+}
+
+WireCell::Opflash::Opflash(TH1F **hist, double start_time, int start_bin, int end_bin, float bin_width)
+  : type(2)
+{
+  low_time = start_time + (start_bin+0.5)*bin_width;
+  high_time = start_time + (end_bin-0.5)*bin_width;
+  
+  for (int i=0;i!=32;i++){
+    PE[i] = 0;
+    PE_err[i] = 0.2; // 0.2 PE as base ... 
+  }
+  
+  double max = 0;
+  int max_bin = 0;
+
+  for (int i=start_bin; i!=end_bin;i++){
+    double peak = 0;
+    for (int j=0;j!=32;j++){
+      double content = hist[j]->GetBinContent(i+1);
+      if (content < 0.2) content = 0;
+      peak += content;
+      PE[j] += content;
+    }
+
+    if (peak > max){
+      max = peak;
+      max_bin = i;
+    }
+  }
+
+  time = start_time + (max_bin+0.5) * bin_width;
+
+  total_PE = 0;
+  for (int i=0;i!=32;i++){
+    PE[i] -= 1.875; // 7.5 us *
+    if (PE[i]<0) PE[i] = 0;
+    total_PE += PE[i];
+
+    if (PE[i]>=2){
+      fired_channels.push_back(i);
+    }
+
+    if (PE[i] < 1000){
+      PE_err[i] = sqrt(pow(PE_err[i],2) + pow(PE[i]*0.02,2)); // standard error below threshold would be 4.6 PE ... 8/sqrt(3)
+    }else if (PE[i] < 2000){
+      PE_err[i] = sqrt(pow(PE_err[i],2) + pow(PE[i]*0.06,2)); // standard error below threshold would be 4.6 PE ... 8/sqrt(3)
+    }else{
+      PE_err[i] = sqrt(pow(PE_err[i],2) + pow(PE[i]*0.18,2)); // standard error below threshold would be 4.6 PE ... 8/sqrt(3)
+    }
+    
+    
+  }
+  
 }
 
 WireCell::Opflash::~Opflash(){
