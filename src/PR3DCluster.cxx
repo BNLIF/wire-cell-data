de44e5817de2a192d056866c8a0b4bf3e65aeb9b
Xin Qian <last.qx@gmail.com>
2018-01-29 08:52:31 -0500

    catch up

--- src/PR3DCluster.cxx
+++ src/PR3DCluster.cxx
@@ -2037,7 +2037,7 @@ bool PR3DCluster::judge_vertex(Point& p_test){
       }
     }
 
-    std::cout << asy << " " << temp_num_occupied_points << " " << temp_num_total_points << " " << temp_num_occupied_points * 1.0 / temp_num_total_points << " " << p_test.x/units::cm << " " << p_test.y/units::cm << " " << p_test.z/units::cm << std::endl;
+    //  std::cout << asy << " " << temp_num_occupied_points << " " << temp_num_total_points << " " << temp_num_occupied_points * 1.0 / temp_num_total_points << " " << p_test.x/units::cm << " " << p_test.y/units::cm << " " << p_test.z/units::cm << std::endl;
 
     if (temp_num_occupied_points < temp_num_total_points * 0.85)
       return true;
