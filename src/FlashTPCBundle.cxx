7062b39dce1d733996f57789fb0197d5bfeaffe5
Xin Qian <last.qx@gmail.com>
2018-03-19 22:59:40 -0400

    tune

--- src/FlashTPCBundle.cxx
+++ src/FlashTPCBundle.cxx
@@ -227,7 +227,7 @@ bool FlashTPCBundle::examine_bundle(Double_t *cos_pe_low, Double_t *cos_pe_mid){
       }
     }
     
-    // if (fabs(main_cluster->get_cluster_id()-14)<=0){
+    // if (fabs(main_cluster->get_cluster_id()-19)<=0){
     //   std::cout << flash->get_flash_id() << " " << main_cluster->get_cluster_id() << " " << nfired << " " << ntot << " " << nfired1 << " " << ntot1 << " " << ks_dis << " " << chi2 << " " << ndf << " " << flag_at_x_boundary << " " << flag_close_to_PMT << std::endl;
     // }
 
