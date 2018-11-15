3521b3c98682f4a7e07cf250afe4aa6bcbd27bcb
Xin Qian <last.qx@gmail.com>
2018-11-15 12:26:01 -0500

    figuring out the geometry ...

--- src/PR3DCluster.cxx
+++ src/PR3DCluster.cxx
@@ -2124,6 +2124,29 @@ void PR3DCluster::dQ_dx_fit(double flash_time){
   double pitch_u = mp.get_pitch_u();
   double pitch_v = mp.get_pitch_v();
   double pitch_w = mp.get_pitch_w();
+  double first_u_dis = mp.get_first_u_dis();
+  double first_v_dis = mp.get_first_v_dis();
+  double first_w_dis = mp.get_first_w_dis();
+  double angle_u = mp.get_angle_u();
+  double angle_v = mp.get_angle_v();
+  double angle_w = mp.get_angle_w();
+  //  convert Z to W ... 
+  double slope_zw = 1./pitch_w * cos(angle_w);
+  double slope_yw = 1./pitch_w * sin(angle_w);
+  
+  double slope_yu = -1./pitch_u * sin(angle_u);
+  double slope_zu = 1./pitch_u * cos(angle_u);
+  double slope_yv = -1./pitch_v * sin(angle_v);
+  double slope_zv = 1./pitch_v * cos(angle_v);
+  //convert Y,Z to U,V
+  double offset_w = -first_w_dis/pitch_w + 0.5;
+  double offset_u = -first_u_dis/pitch_u + 0.5;
+  double offset_v = -first_v_dis/pitch_v + 0.5;
+
+  double first_t_dis = path_wcps.front().mcell->GetTimeSlice()*time_slice_width - path_wcps.front().x;
+  double slope_xt = 1./time_slice_width;
+  double offset_t =  first_t_dis/time_slice_width + 0.5;
+  
   // get the correct flash time matching TPC side
   flash_time = flash_time - time_tick_offset*0.5*units::microsecond; // us
   // given an x position value, we want to convert this to a drift time 
@@ -2141,13 +2164,32 @@ void PR3DCluster::dQ_dx_fit(double flash_time){
   double ind_sigma_u_T = 0.402993 * pitch_u; // units::mm
   double ind_sigma_v_T = 0.402993 * pitch_v; // units::mm
   
-  // these is the longitudinal filters in the wire dimension ...
+  // these is the longitudinal filters in the time dimension ...
   double sigma_L = 1.13656 * time_slice_width * units::cm / nrebin / 0.5; // units::mm 
-  
-  
+
+
+  // Now start the fit ... 
+  Point reco_pos(150*units::cm,30*units::cm,600*units::cm);
+  reco_pos.x = (reco_pos.x + 0.6*units::cm)/1.098 * 1.101 - 1*0.1101*units::cm ;
+  // reco_pos.z += 1.5*units::mm;
+  // reco_pos.y -= 1.5*units::mm;
+  // reco_pos.x =  (reco_pos.x+0.1101*units::cm*2)/1.1009999*1.098 - 0.6*units::cm;
+  std::cout << reco_pos.x/units::cm << " " << reco_pos.y/units::cm << " " << reco_pos.z/units::cm << std::endl;
+
+  // let's convert these into time and wire numbers 
+
+  // T U, V, W, wires ...
+  std::cout << offset_t + slope_xt * reco_pos.x  << std::endl;
+  std::cout << offset_u + (slope_yu * reco_pos.y + slope_zu * reco_pos.z) << std::endl;
+  std::cout << offset_v + (slope_yv * reco_pos.y + slope_zv * reco_pos.z) + 2400 << std::endl;
+  std::cout << offset_w + (slope_yw * reco_pos.y + slope_zw * reco_pos.z) + 4800 << std::endl;
+  // these should be the expected values:
+  // U, V, W, T,  1252.01, 3819.54, 6799.62, 1485.81
+  // reco position 1252.02, 3819.63, 6799.67, 1485.79
+  // good ...
 
   
-  //  double first_t_dis = path_wcps.front().mcell->GetTimeSlice()*time_slice_width - path_wcps.front().x;
+  //  
   //  std::cout << first_t_dis/units::cm << " " << time_slice_width/units::cm << std::endl; 
   // double offset_x = (flash_time - time_tick_offset)*2./nrebin*time_slice_width;
 
@@ -2487,7 +2529,7 @@ void PR3DCluster::fine_tracking(int num_pts_cut){
   // T = slope_x * ( x + offset_x);
   double slope_x = 1./time_slice_width;
   double first_t_dis = path_wcps_vec.at(0).mcell->GetTimeSlice()*time_slice_width - path_wcps_vec.at(0).x;
-  double offset_t = first_t_dis / time_slice_width;
+  double offset_t = first_t_dis / time_slice_width + 0.5;
   
   
   
@@ -2499,10 +2541,11 @@ void PR3DCluster::fine_tracking(int num_pts_cut){
   double slope_zu = 1./pitch_u * cos(angle_u);
   double slope_yv = -1./pitch_v * sin(angle_v);
   double slope_zv = 1./pitch_v * cos(angle_v);
+  
   //convert Y,Z to U,V
-  double offset_w = -first_w_dis/pitch_w;
-  double offset_u = -first_u_dis/pitch_u;
-  double offset_v = -first_v_dis/pitch_v;
+  double offset_w = -first_w_dis/pitch_w + 0.5;
+  double offset_u = -first_u_dis/pitch_u + 0.5;
+  double offset_v = -first_v_dis/pitch_v + 0.5;
  
   //  for (size_t i=0;i!=path_wcps_vec.size();i++){
     //std::cout << i << " " << path_wcps_vec.at(i).mcell->GetTimeSlice() - offset_t << " " << slope_x * path_wcps_vec.at(i).x << " " << path_wcps_vec.at(i).mcell->GetTimeSlice() - offset_t - slope_x * path_wcps_vec.at(i).x << std::endl;
