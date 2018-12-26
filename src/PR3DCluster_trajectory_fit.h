c0c6bae071726046eeea0a9627e02ec9ca107622
Xin Qian <last.qx@gmail.com>
2018-12-26 12:41:53 -0500

    add a flag

--- src/PR3DCluster_trajectory_fit.h
+++ src/PR3DCluster_trajectory_fit.h
@@ -242,7 +242,7 @@ void PR3DCluster::form_map_graph_based(std::vector<WCPointCloud<double>::WCPoint
 }
 
 
-void PR3DCluster::trajectory_fit(PointVector& ps_vec, double first_t_dis, std::vector<double>& distances,std::map<int,std::set<std::pair<int,int>>>& map_3D_2DU_set, std::map<int,std::set<std::pair<int,int>>>& map_3D_2DV_set, std::map<int,std::set<std::pair<int,int>>>& map_3D_2DW_set, std::map<std::pair<int,int>,std::set<int>>& map_2DU_3D_set, std::map<std::pair<int,int>,std::set<int>>& map_2DV_3D_set, std::map<std::pair<int,int>,std::set<int>>& map_2DW_3D_set,std::map<std::pair<int,int>,double>& map_2D_ut_charge,std::map<std::pair<int,int>,double>& map_2D_ut_charge_err, std::map<std::pair<int,int>,double>& map_2D_vt_charge,std::map<std::pair<int,int>,double>& map_2D_vt_charge_err, std::map<std::pair<int,int>,double>& map_2D_wt_charge,std::map<std::pair<int,int>,double>& map_2D_wt_charge_err){
+void PR3DCluster::trajectory_fit(PointVector& ps_vec, double first_t_dis, std::vector<double>& distances,std::map<int,std::set<std::pair<int,int>>>& map_3D_2DU_set, std::map<int,std::set<std::pair<int,int>>>& map_3D_2DV_set, std::map<int,std::set<std::pair<int,int>>>& map_3D_2DW_set, std::map<std::pair<int,int>,std::set<int>>& map_2DU_3D_set, std::map<std::pair<int,int>,std::set<int>>& map_2DV_3D_set, std::map<std::pair<int,int>,std::set<int>>& map_2DW_3D_set,std::map<std::pair<int,int>,double>& map_2D_ut_charge,std::map<std::pair<int,int>,double>& map_2D_ut_charge_err, std::map<std::pair<int,int>,double>& map_2D_vt_charge,std::map<std::pair<int,int>,double>& map_2D_vt_charge_err, std::map<std::pair<int,int>,double>& map_2D_wt_charge,std::map<std::pair<int,int>,double>& map_2D_wt_charge_err, int flag_lambda, double init_lambda){
   
   // map 2D points to its index
   std::vector<std::pair<std::pair<int,int>,int>> vec_2DU_index;
@@ -381,14 +381,7 @@ void PR3DCluster::trajectory_fit(PointVector& ps_vec, double first_t_dis, std::v
   Eigen::SparseMatrix<double> RVT = Eigen::SparseMatrix<double>(RV.transpose());
   Eigen::SparseMatrix<double> RWT = Eigen::SparseMatrix<double>(RW.transpose());
 
-   double lambda = 2 // strength 
-    * sqrt(9. //  average chi2 guessted
- 	   * (vec_2DU_index.size() + vec_2DV_index.size() + vec_2DW_index.size()) // how many of them
- 	   * 6 * 6 // charge/charge_err estimation ... 
- 	   /(ps_vec.size() * 1.)); //weighting
-  lambda = 0;
-  //double dis_range = 0.5*units::cm/sqrt(3.);
-  double angle_range = 0.25;
+  
  
   //std::cout << lambda/ dis_range << std::endl;
   
@@ -427,7 +420,21 @@ void PR3DCluster::trajectory_fit(PointVector& ps_vec, double first_t_dis, std::v
     }
   }
 
-  
+  double lambda = 0;
+
+  if (flag_lambda ==0){
+    lambda = 0;
+  }else if (flag_lambda==1){
+    lambda = init_lambda;
+  }else if (flag_lambda==2){
+    lambda = 2 // strength 
+    * sqrt(9. //  average chi2 guessted
+ 	   * (vec_2DU_index.size() + vec_2DV_index.size() + vec_2DW_index.size()) // how many of them
+ 	   * 6 * 6 // charge/charge_err estimation ... 
+ 	   /(ps_vec.size() * 1.)); //weighting
+  }
+  //double dis_range = 0.5*units::cm/sqrt(3.);
+  double angle_range = 0.25;
   FMatrix *= lambda/angle_range ; // disable the angue cut ... 
   
   
